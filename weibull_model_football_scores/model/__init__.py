b1c3c8149ad8c8bd3f525bd4b7dc156a8a8924e6
Manuel Offidani <manuel.offidani.ff@pirelli.com>
2023-04-07 11:23:38 +0100

    Bug fix

--- weibull_model_football_scores/model/__init__.py
+++ weibull_model_football_scores/model/__init__.py
@@ -23,26 +23,7 @@ class WeibullCountFitter:
         _lambda = a + b
         if home:
             _lambda += self.gamma
-        return _lambda
-
-    def _get_current_likelihood(self):
-        _likelihood = 0
-        for i, row in self.df.iterrows():
-            home_team = row["HomeTeam"]
-            away_team = row["AwayTeam"]
-            home_goals = row["FTHG"]
-            away_goals = row["FTAG"]
-
-            l1 = self._calculate_lambda(
-                self.C[home_team]["alpha"], self.C[away_team]["beta"], home=True
-            )
-            l2 = self._calculate_lambda(
-                self.C[home_team]["beta"], self.C[away_team]["alpha"], home=False
-            )
-
-            _likelihood += weibull_likelihood(home_goals, away_goals, l1, l2)
-
-        return _likelihood
+        return np.exp(_lambda)
 
     @property
     def team_mapping(self):
@@ -61,13 +42,14 @@ class WeibullCountFitter:
         return X
 
     def _initialise_coefficients(self):
-        self.C = abs(np.random.normal(0, 1, (len(self.teams), 2)))
+        self.C = 0.1 * (np.random.normal(0, 1, (len(self.teams), 2)))
 
-    def _get_likelihood(self, C: np.array):
-        df = self.df.copy()
+    def _get_likelihood(self, df: pd.DataFrame, C: np.array) -> float:
         log_l = 0
 
         max_fixture = df["fixture"].max()
+
+        import time
 
         for i, row in df.iterrows():
             home = row["HomeTeam"]
@@ -75,6 +57,17 @@ class WeibullCountFitter:
             i, j = self.team_mapping[home], self.team_mapping[away]
             l1 = self._calculate_lambda(C[i][0], C[j][1], home=True)
             l2 = self._calculate_lambda(C[j][0], C[i][1], home=False)
+            #   print(l1, l2, row["FTHG"], row["FTAG"])
+            #   print(
+            #       np.exp(-config.XI * (max_fixture - row["fixture"])),
+            #       weibull_likelihood(y1=row["FTHG"], y2=row["FTAG"], l1=l1, l2=l2),
+            #   )
+            #   print(
+            #       np.log(
+            #           np.exp(-config.XI * (max_fixture - row["fixture"]))
+            #           * weibull_likelihood(y1=row["FTHG"], y2=row["FTAG"], l1=l1, l2=l2)
+            #       )
+            #   )
             log_l += np.log(
                 np.exp(-config.XI * (max_fixture - row["fixture"]))
                 * weibull_likelihood(y1=row["FTHG"], y2=row["FTAG"], l1=l1, l2=l2)
@@ -82,8 +75,9 @@ class WeibullCountFitter:
 
         return log_l
 
-    def _get_grads(self, C: np.array):
+    def _get_grads(self, df: pd.DataFrame, C: np.array):
         C = C.copy()
+        df = df.copy()
         eps = 1e-6
 
         grads = np.zeros_like(C)
@@ -95,40 +89,54 @@ class WeibullCountFitter:
                 C_plus[i, j] += eps
                 C_minus[i, j] -= eps
                 grads[i, j] = (
-                    self._get_likelihood(C_plus) - self._get_likelihood(C_minus)
+                    self._get_likelihood(df, C_plus) - self._get_likelihood(df, C_minus)
                 ) / (2 * eps)
 
         return grads
 
     def fit(
         self,
-        df: pd.DataFrame,
+        train: pd.DataFrame,
         n_iter: int = 100,
         learning_rate: float = 0.001,
         verbose: bool = True,
         save_model: bool = True,
+        test: pd.DataFrame = None,
     ):
-        self.df = df.copy()
-        self._get_all_teams(df)
+        train = train.copy()
+        self._get_all_teams(train)
         self._initialise_coefficients()
 
-        log_l = self._get_likelihood(self.C)
-        if verbose:
-            print(f"Starting likelihood: {log_l}")
+        train_log_l = self._get_likelihood(train, self.C)
+        if test is not None:
+            test_log_l = self._get_likelihood(test, self.C)
 
-        self.likelihoods = [log_l]
+        if verbose:
+            print(f"Starting train likelihood: {train_log_l}")
+            if test is not None:
+                print(f"Starting test likelihood: {test_log_l}")
+
+        self.train_likelihoods = [train_log_l]
+        if test is not None:
+            self.test_likelihoods = [test_log_l]
 
         C = self.C.copy()
         for n in range(n_iter):
-            C += learning_rate * self._get_grads(C)
+            # C += learning_rate * self._get_grads(train, C)
+            C += learning_rate * self._get_grads(train, C)
             self.C = C.copy()
-            log_l = self._get_likelihood(self.C)
-            self.likelihoods.append(log_l)
+            train_log_l = self._get_likelihood(train, self.C)
+            self.train_likelihoods.append(train_log_l)
+            if test is not None:
+                test_log_l = self._get_likelihood(test, self.C)
+                self.test_likelihoods.append(test_log_l)
 
             if verbose:
-                print(f"Step {n + 1}, current likelihood: {log_l}")
+                print(f"Step {n + 1}, current likelihood: {train_log_l}")
+                if test is not None:
+                    print(f"Step {n + 1}, current test likelihood: {test_log_l}")
 
-            if self.likelihoods[-1] - self.likelihoods[-2] < 10e-4:
+            if self.train_likelihoods[-1] - self.train_likelihoods[-2] < 10e-4:
                 print("Early stop. Algorithm has converged")
                 break
 
